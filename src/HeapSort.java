8ab8d95c7034a035f3b96b176a3b55526710fdac
Tchiho <Tchiho@outlook.com>
2022-03-28 17:05:58 +0800

    Add new code

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,5 +1,10 @@
 public class HeapSort {
-
+    /**
+     * 产生大顶堆的堆调整算法
+     * @param arr 数组
+     * @param pointer 指针
+     * @param length 遍历长度
+     */
     public static void adjustHeap(int[] arr, int pointer, int length){
         int temp = arr[pointer];
         for(int newp = pointer * 2 + 1; newp < length; newp = newp * 2 + 1){
@@ -14,6 +19,10 @@ public class HeapSort {
         }
     }
 
+    /**
+     * 将大顶堆调整为顺序数组
+     * @param arr 数组
+     */
     public static void heapSort(int[] arr){
         for(int i = arr.length / 2 - 1; i >= 0; i --){
             adjustHeap(arr, i, arr.length);
--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,10 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Hello {
+
     public static void main(String[] args){
         System.out.print("This is a test module\n");
-        int[] arr = {9, 5, 3, 4, 6, 7, 1, 8, 2, 0};
+        int[] arr = {9, 5, 7, 3, 4, 6, 7, 1, 8, 2, 0, -6, 12, };
         System.out.println("修改之前：" + Arrays.toString(arr));
         HeapSort.heapSort(arr);
         System.out.println("修改之后：" + Arrays.toString(arr));
